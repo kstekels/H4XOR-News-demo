b04f0e28c3f562336fe474b32dd08f7b01cdba28
Karlis <karlis.developer@gmail.com>
2021-03-31 15:13:06 +0300

    Initial Commit

---
+++ H4XOR News demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News demo
+//
+//  Created by karlis.stekels on 31/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
